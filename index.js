fe0d3a30ef6410f934fb96e2b01274ec25b1e02b
bdimon <dmytr0b@yahoo.com>
2024-08-09 21:44:35 +0200

    edit vercel.json

--- index.js
+++ index.js
@@ -12,20 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-// bot.setMyCommands([
-//     {command: '/start', description: 'Start of my bot'},
-//     {command: '/info', description: 'Get info about user'},
-//     {command: '/help', description: 'User can call to me by phone'},
-
-// ])
-
 const start = () => {
     bot.on('message', async (msg) => {
         const text = msg.text;
         const chatId = msg.chat.id;
         if (text === '/start') {
-            // await bot.sendSticker(chatId, 'https://sl.combot.org/prog1/webp/5xf09f988e.webp');
 
             await bot.sendMessage(chatId, 'Please fill form on the bottom', {
                 reply_markup: {
@@ -44,21 +35,7 @@ const start = () => {
                 }
             });
 
-            // await bot.sendMessage(chatId, 'Welcome to Bot Store of Anna hand made!')
-            // Для того, чтобы завершить задачу
-            // return bot.sendMessage(chatId, 'Welcome to Bot Store of Anna hand made!')
         }
-
-        // if (text === '/info') {
-        //     // await bot.sendMessage(chatId, `Hello ${msg.from.first_name} ${msg.from.last_name}`)
-        //     // Для того, чтобы завершить задачу
-        //     return bot.sendMessage(chatId, `Hello ${msg.from.first_name}!`)
-        // }
-        // if (text === '/help') {
-        //     // await bot.sendMessage(chatId, `Hello ${msg.from.first_name} ${msg.from.last_name}`)
-        //     // Для того, чтобы завершить задачу
-        //     return bot.sendMessage(chatId, `${msg.from.first_name}, you can call me for emergency help`)
-        // }
 
         if (msg?.web_app_data?.data) {
           try {
@@ -77,7 +54,6 @@ const start = () => {
           }
 
       }
-        // return bot.sendMessage(chatId, 'I don\'t understand you, sorry...');
     });
 }
 
